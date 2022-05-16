a77b773d437dd603a958a19774644efc9b218d33
Paulo Nascimento <pnascimento@bestpontos.com.br>
2022-05-16 11:22:26 -0300

    initial commit

---
+++ src/main/java/com/phcardoso/webhookflux/WebhookfluxApplication.java
@@ -0,0 +1,13 @@
+package com.phcardoso.webhookflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phcardoso/webhookflux/WebhookfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phcardoso.webhookflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
